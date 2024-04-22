06ae3a69bb088580b4a6095a6f5a6a7e26d1f1ef
rahul nayak <rahul_nayak@thbs.com>
2024-04-22 14:57:00 +0530

    add feature verify e-ticket

--- src/main/java/com/thbs/backend/Controllers/QRDecodeController.java
+++ src/main/java/com/thbs/backend/Controllers/QRDecodeController.java
@@ -1,9 +1,9 @@
 package com.thbs.backend.Controllers;
-
+ 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+ 
 import com.thbs.backend.Models.AttendeeList;
 import com.thbs.backend.Models.Event;
 import com.thbs.backend.Models.UserModel;
@@ -22,65 +22,70 @@ import com.thbs.backend.Repositories.EventRepo;
 import com.thbs.backend.Repositories.UserRepo;
 import com.thbs.backend.Services.AESEncryptionService;
 import com.thbs.backend.Services.AuthService;
-
+ 
 @RestController
 @RequestMapping("api")
 public class QRDecodeController {
-
+ 
     @Autowired
     private AESEncryptionService encryptDecryptService;
-
+ 
     @Autowired
     private AuthService authService;
-
+ 
     @Autowired
     private AttendeeRepo attendeeRepo;
-
+ 
     @Autowired
     private UserRepo userRepo;
-
+ 
     @Autowired
     private EventRepo eventRepo;
-
+ 
     @Autowired
     private EventProviderRepo eventProviderRepo;
-    
-
+   
+ 
     @GetMapping("scan-qr")
     public ResponseEntity<String> scanQRCode(@RequestParam String encryptedData,
             @RequestHeader String token) {
         try {
             String email = authService.verifyToken(token);
             UUID organizerId = eventProviderRepo.findByEmail(email).getId();
-
+ 
             // Decrypt the QR code data
             System.out.println("Encrypted data: " + encryptedData); // Debugging
             String encryptedData_without_space = encryptedData.replaceAll(" ", "+");
             System.out.println(encryptedData_without_space);
             String decryptedData = encryptDecryptService.decryptData(encryptedData_without_space);
-
+ 
             if (decryptedData == null) {
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to decrypt data");
             }
-
+ 
             System.out.println("Decrypted data: " + decryptedData); // Debugging
-
+ 
             String[] outputData = decryptedData.split(",");
             String user_id = outputData[0];
             String event_id = outputData[1];
             System.out.println("user_id : " + user_id + " event_id : " + event_id);
-
-
+ 
+ 
             Event eventFromDB = eventRepo.findByEventId(UUID.fromString(event_id));
-            if (eventFromDB.getEventOrgId()!=organizerId) {
+            System.out.println("Organizer Id from qr : "+ organizerId);
+            System.out.println("Organizer Id from event db : "+ eventFromDB.getEventOrgId());
+            System.out.println(eventFromDB.getEventOrgId()!=organizerId);
+            if (!eventFromDB.getEventOrgId().equals(organizerId)) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Event ID is not valid for the organizer");
             }
-
+           
+           
+ 
             // List<UUID> eventIds = eventRepo.findEventIdsByOrganizerId(organizerId);
             // if (!eventIds.contains(UUID.fromString(event_id))) {
             //     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Event ID is not valid for the organizer");
             // }
-
+ 
             Optional<UserModel> userDetails = userRepo.findById(UUID.fromString(user_id));
             if (userDetails.isPresent()) {
                 UserModel user = userDetails.get();
@@ -90,24 +95,23 @@ public class QRDecodeController {
                 attendee.setEmail(user.getEmail());
                 attendee.setEventId(UUID.fromString(event_id));
                 attendeeRepo.save(attendee);
-
+ 
             } else {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
             }
-
-            // Further processing...
-
+ 
+ 
             return ResponseEntity.ok("Decryption successful user: " + user_id + " event: " + event_id);
-
+ 
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Failed to update attendee list. Reason: " + e.getMessage());
         }
     }
-
+ 
     @GetMapping("/getAttendeeList")
     public List<AttendeeList> getAttendeeList(@RequestHeader UUID eventId) {
         return attendeeRepo.findByEventId(eventId);
     }
-
+ 
 }
--- src/main/java/com/thbs/backend/Repositories/UserRepo.java
+++ src/main/java/com/thbs/backend/Repositories/UserRepo.java
@@ -1,12 +1,16 @@
 package com.thbs.backend.Repositories;
-
+ 
 import java.util.UUID;
-
+ 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+ 
 import com.thbs.backend.Models.UserModel;
-
+import java.util.Optional;
+ 
+ 
 public interface UserRepo extends JpaRepository<UserModel,UUID>{
     UserModel findByEmail(String email);
-    
+    Optional<UserModel> findById(UUID id);
+   
+   
 }
